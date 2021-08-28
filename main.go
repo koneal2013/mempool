32a63b24e54e161723672d4ffa30211b14f188d7
koneal2013 <koneal2013@gmail.com>
2021-08-28 18:57:59 -0400

    adding transactions.txt adding file processing logic

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/joho/godotenv"
 	"kava-challange/pkg/constants"
 	"kava-challange/pkg/logging"
 	"kava-challange/pkg/types"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -20,8 +23,37 @@ func main() {
 	} else {
 		logger.Sugar().Info("retrieving transactions and inserting into mempool")
 		mempool := types.NewMempool(maxPoolSize, logger)
-		mempool.AddTx(types.NewTx("hash", "sig", 542.55, 1.01))
-
+		//Process transactions.txt and insert into mempool
+		if transactionFile, err := os.Open(os.Getenv(constants.ENV_TRANSACTIONS_FILE_PATH)); err != nil {
+			logger.Sugar().Errorf("error opening transactions.txt. ensure [%s] enviornment variable is set", constants.ENV_TRANSACTIONS_FILE_PATH)
+		} else {
+			defer transactionFile.Close()
+			scanner := bufio.NewScanner(transactionFile)
+			scanner.Split(bufio.ScanLines)
+			currentLine := 0
+			for scanner.Scan() {
+				currentLine++
+				rawTransaction := strings.Fields(scanner.Text())
+				if len(rawTransaction) != 4 {
+					logger.Sugar().Errorf("transaction file at path [%s] is misformatted at line [%v]", constants.ENV_TRANSACTIONS_FILE_PATH, currentLine)
+					continue
+				}
+				txHash := strings.TrimPrefix(rawTransaction[0], "TxHash=")
+				if gas, err := strconv.ParseFloat(strings.TrimPrefix(rawTransaction[1], "Gas="), 64); err != nil {
+					logger.Sugar().Errorf("gas conversion error for transaction with hash [%s] on line [%v]", txHash, currentLine)
+					continue
+				} else if feePerGas, err := strconv.ParseFloat(strings.TrimPrefix(rawTransaction[2], "FeePerGas="), 64); err != nil {
+					logger.Sugar().Errorf("feePerGas conversion error for transaction with hash [%s] on line [%v]", txHash, currentLine)
+					continue
+				} else {
+					signature := strings.TrimPrefix(rawTransaction[3], "Signature=")
+					if err = mempool.AddTx(types.NewTx(txHash, signature, gas, feePerGas)); err != nil {
+						logger.Sugar().Error(err)
+					}
+				}
+			}
+			fmt.Println(len(mempool.Transactions))
+		}
 	}
 	logger.Sugar().Named("main").Info("Done...")
 }
